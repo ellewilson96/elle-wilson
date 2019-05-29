c128cf58c73c4339ab4057ae4b7d8774a424178a
ellewilson96 <eleanorwilsonn@gmail.com>
2019-05-29 01:46:07 -0400

    add tons of css formatting to text and layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,14 @@ import { Link } from 'react-router-dom'
     return(
       <div className="footer-container">
       <div className="profile">
-      <img src={profile} className="profile" alt="me" />
+      <img src={profile} alt="me" />
       </div>
       <div className="social-links">
-        <Link to="https://www.linkedin.com/in/eleanormwilson/">LinkedIn | </Link>
-        <Link to="https://www.instagram.com/elle_wilson">Instagram | </Link>
-        <Link to="https://www.github.com/ellewilson96/">Github</Link>
-
+      <ul>
+        <li><Link to="https://www.linkedin.com/in/eleanormwilson/">LinkedIn </Link></li>
+        <li><Link to="https://www.instagram.com/elle_wilson">Instagram </Link></li>
+        <li><Link to="https://www.github.com/ellewilson96/">Github</Link></li>
+      </ul>
       </div>
       </div>
   )}
