524000357e91fbf75a6cb73e720fe3e442b01418
ellewilson96 <eleanorwilsonn@gmail.com>
2019-05-30 17:48:12 -0400

    fix about page on mobile display

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ import sign from '../img/sign.png'
             Fashion Institute of Technology. There, she obtained a Bachelor of Science degree in Fashion Business Management with a minor in International
               Trade & Marketing and a GPA of 3.75.</p>
       <p>	In December of 2017, her senior year of college, Elle discovered an interest in web development after using
-          Salesforce as a tool for clienteling at her current role at
+          Salesforce as a tool for clienteling at her previous role at
             Opening Ceremony.
           She began researching educational programs and enrolled in The Flatiron School's
             Full-Stack Software Engineering online immersive. While committing full-time hours every week as both a supervisor and a student,
