753de34fd5a6cb257e8ae0f9050b3d6afe04cc77
ellewilson96 <eleanorwilsonn@gmail.com>
2019-06-10 15:04:23 -0400

    update about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/About.css'
-import profile from '../img/profile.png'
-import sign from '../img/sign.png'
+import one from '../img/about/1.jpg'
+import two from '../img/about/2.jpg'
+import three from '../img/about/3.jpg'
+import four from '../img/about/4.jpg'
+import five from '../img/about/5.jpg'
+import six from '../img/about/6.jpg'
+
 
   class About extends Component {
 
@@ -9,26 +14,16 @@ import sign from '../img/sign.png'
     return(
       <div className="about-container">
       <div className="title">
-        <h2>About</h2>
+        <h2>My Story</h2>
       </div>
         <div className="about-content">
-      <p>Elle Wilson has been working in the fashion industry for over 6 years. Originally from England,
-          Elle relocated with her family to New Jersey in 2004 before moving to New York City in 2014 to attend the
-            Fashion Institute of Technology. There, she obtained a Bachelor of Science degree in Fashion Business Management with a minor in International
-              Trade & Marketing and a GPA of 3.75.</p>
-      <p>	In December of 2017, her senior year of college, Elle discovered an interest in web development after using
-          Salesforce as a tool for clienteling at her previous role at
-            Opening Ceremony.
-          She began researching educational programs and enrolled in The Flatiron School's
-            Full-Stack Software Engineering online immersive. While committing full-time hours every week as both a supervisor and a student,
-            Elle worked remotely to complete the program in January of 2019.</p>
-          </div>
-          <div className="profile-image">
-          <img src={profile} alt="me" />
-          </div>
-          <div className="sign">
-          <img src={sign} alt="elle" />
-          </div>
+        <img src={one} alt="" />
+        <img src={two} alt="" />
+        <img src={three} alt="" />
+        <img src={four} alt="" />
+        <img src={five} alt="" />
+        <img src={six} alt="" />
+        </div>
     </div>
   )}
 }
